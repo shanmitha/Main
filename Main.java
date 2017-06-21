943bc26b70e26dc23321669c0f1f692ed6aca2ad
shanmitha <shanmithasekar@gmail.com>
2017-06-21 11:50:55 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class Main {
+	public int find(int [] arrA){
+		int index = -1;
+		HashSet<Integer> hs = new HashSet<>();
+		for(int i = arrA.length-1;i>=0;i--){
+			if(hs.contains(arrA[i])){
+				index = i;
+			}else{
+				hs.add(arrA[i]);
+			}
+		}
+		return arrA[index];
+	}
+	public static void main(String args[]){
+		int [] a = {1,2,5,7,5,3,10,2};
+		Main f = new Main();
+		System.out.println("{1,2,5,7,5,3,10,2}");
+		System.out.println("first repeated element by index is : " + f.find(a));
+	}
+}
